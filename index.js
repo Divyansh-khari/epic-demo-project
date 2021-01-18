8fb8e40b9a78d7b3b3c6ad562524e249d21305a4
Divyansh Khari <Divyansh@office.epicsafety.com>
2021-01-18 10:22:44 -0800

    Website

--- index.js
+++ index.js
@@ -54,7 +54,10 @@ app.post('/login', async(req,res)=>{
   if(results.rows[0].name==user && results.rows[0].password==password){
    res.render('pages/image');
  }
- 
+ else{
+   res.send("<h2>You have either not Registered Yourself or entered a Wrong Password or Username!!</h2")
+ }
+
   })
 
 });
